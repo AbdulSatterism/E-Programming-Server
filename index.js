563cc1419c7f7e112561f905f8aa4f89bde88012
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2023-02-26 21:50:04 +0600

    courses link create ok

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/Courses.json')
+
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('welcome to our e-programming')
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
 
 app.listen(port, () => {
     console.log('port is running', port)
